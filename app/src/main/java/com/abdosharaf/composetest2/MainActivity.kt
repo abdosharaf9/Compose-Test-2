e3ba1146bbfeb8bd7a2bfbc1c59c22f76d05edfa
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-21 15:45:48 +0200

    Rows & Columns

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,13 +3,25 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.abdosharaf.composetest2.ui.theme.ComposeTest2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +34,79 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeTest2Theme {
-        Greeting("Android")
+        /*Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .weight(1f),
+                color = MaterialTheme.colorScheme.primary
+            ) {}
+
+            Surface(
+                modifier = Modifier
+                    .width(200.dp)
+                    .weight(3f),
+                color = MaterialTheme.colorScheme.secondary
+            ) {}
+        }*/
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            /*Surface(
+                modifier = Modifier
+                    .height(50.dp)
+                    .weight(1f),
+                color = MaterialTheme.colorScheme.primary
+            ) {}
+
+            Surface(
+                modifier = Modifier
+                    .height(50.dp)
+                    .weight(3f),
+                color = MaterialTheme.colorScheme.secondary
+            ) {}*/
+            SurfaceItem(weight = 3f, MaterialTheme.colorScheme.secondary)
+            SurfaceItem(weight = 1f)
+        }
     }
+}
+
+@Composable
+fun RowScope.SurfaceItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(
+        modifier = Modifier
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
+
+@Composable
+fun ColumnScope.SurfaceItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
