27b616c89f6510a98068c45718cb12d990a5298b
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-22 23:55:27 +0200

    Coil - Image Loading Library

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,12 +3,29 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.util.TypedValueCompat.dpToPx
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.ImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,9 +45,51 @@ fun DefaultPreview() {
 @Composable
 fun MainScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        GoogleButton(
-            text = "Sign Up with Google",
-            loadingText = "Loading...."
+        /*val painter = rememberImagePainter(data = "https://picsum.photos/600/600", builder = {
+//            placeholder(R.drawable.ic_launcher_foreground)
+            error(R.drawable.ic_launcher_background)
+            crossfade(1000)
+            transformations(
+//                GrayscaleTransformation()
+//                CircleCropTransformation()
+//                BlurTransformation()
+                RoundedCornersTransformation(50f)
+            )
+        })
+        val painterState = painter.state
+
+        Image(painter = painter, contentDescription = "Image", modifier = Modifier.size(200.dp))
+
+        if (painterState is AsyncImagePainter.State.Loading) {
+            CircularProgressIndicator()
+        }*/
+
+//        AsyncImage(model = "https://picsum.photos/600/600", contentDescription = "Image")
+
+        /*AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://picsum.photos/600/600")
+                .crossfade(true)
+                .placeholder(R.drawable.ic_launcher_background)
+                .transformations(
+                    RoundedCornersTransformation(50f)
+                )
+                .size(400)
+                .build(),
+            contentDescription = "Image"
+        )*/
+
+        val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://picsum.photos/600/600")
+                .crossfade(true)
+                .placeholder(R.drawable.ic_launcher_background)
+                .transformations(
+                    RoundedCornersTransformation(50f)
+                )
+                .size(400)
+                .build()
         )
+        Image(painter = painter, contentDescription = "Image")
     }
 }
