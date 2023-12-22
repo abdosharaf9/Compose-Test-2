fc82ca9a008335af449faf601cc43386393af68f
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-22 03:10:33 +0200

    Text Customization

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -24,6 +25,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,20 +60,70 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeTest2Theme {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
-            Box(
-                modifier = Modifier
-                    .background(Color.Blue)
-                /*.width(100.dp)
-                .height(100.dp)
-                .verticalScroll(rememberScrollState())*/,
-                contentAlignment = Alignment.Center
-            ) {
-                Box(modifier = Modifier
-                    .background(Color.Green)
-                    .size(30.dp))
-                Text(text = "I Love Android!", fontSize = 40.sp)
-            }
+        Column(modifier = Modifier.fillMaxSize()) {
+            CustomText3()
         }
     }
+}
+
+@Composable
+private fun CustomText() {
+    Text(
+//                text = "Hello World!",
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+            .width(300.dp),
+        color = Color.White,
+//                fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+        fontSize = 30.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+//                style = MaterialTheme.typography.headlineLarge
+    )
+}
+
+@Composable
+private fun CustomText2() {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("A")
+                }
+
+                append("B")
+
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("D")
+                }
+
+                append("O")
+            }
+        },
+        modifier = Modifier.width(300.dp)
+    )
+}
+
+@Composable
+fun CustomText3() {
+    Text(
+        text = "I Love Android! ".repeat(20),
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis
+    )
 }
