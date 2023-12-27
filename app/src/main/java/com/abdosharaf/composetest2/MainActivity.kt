a0a8f07121791aa3921ec6ce89dd9b4ba16b5324
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-27 18:27:08 +0200

    Custom UI Component - Canvas - Part 1

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -5,13 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,19 +28,6 @@ fun DefaultPreview() {
 @Composable
 fun MainScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        var text by remember {
-            mutableStateOf("")
-        }
-        val maxChar = 10
-
-        OutlinedTextField(
-            value = text,
-            onValueChange = {
-                if (it.length <= maxChar) text = it
-            },
-            maxLines = 1,
-            label = { Text(text = "Name") },
-            placeholder = { Text(text = "Enter Your Name") }
-        )
+        CustomComponent()
     }
 }
