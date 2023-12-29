e50b2261cffc725c0be15ff4bd2d1a328f1e009c
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-29 22:05:23 +0200

    Custom UI Component - Canvas - Part 2

--- app/src/main/java/com/abdosharaf/composetest2/CustomComponent.kt
+++ app/src/main/java/com/abdosharaf/composetest2/CustomComponent.kt
@@ -1,10 +1,20 @@
 package com.abdosharaf.composetest2
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -30,7 +40,30 @@ fun CustomComponent(
     maxValue: Int = 100,
     backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
     backgroundIndicatorStrokeWidth: Float = 100f,
+    foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f,
 ) {
+    var allowedIndicatorValue by remember {
+        mutableIntStateOf(maxValue)
+    }
+
+    allowedIndicatorValue = if (indicatorValue <= maxValue) indicatorValue else maxValue
+
+    var animatedIndicatorValue by remember {
+        mutableFloatStateOf(0f)
+    }
+
+    LaunchedEffect(key1 = allowedIndicatorValue) {
+        animatedIndicatorValue = allowedIndicatorValue.toFloat()
+    }
+
+    val percentage = (animatedIndicatorValue / maxValue) * 100
+    val sweepAngle by animateFloatAsState(
+        targetValue = (2.4 * percentage).toFloat(),
+        animationSpec = tween(durationMillis = 1000),
+        label = ""
+    )
+
     Column(
         modifier = Modifier
             .size(canvasSize)
@@ -42,6 +75,13 @@ fun CustomComponent(
                     componentSize = componentSize,
                     indicatorColor = backgroundIndicatorColor,
                     indicatorStrokeWidth = backgroundIndicatorStrokeWidth
+                )
+
+                foregroundIndicator(
+                    sweepAngle = sweepAngle,
+                    componentSize = componentSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth
                 )
             }
     ) {}
@@ -57,6 +97,29 @@ fun DrawScope.backgroundIndicator(
         color = indicatorColor,
         startAngle = 150f,
         sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            width = indicatorStrokeWidth,
+            cap = StrokeCap.Round
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2,
+            y = (size.height - componentSize.height) / 2
+        )
+    )
+}
+
+fun DrawScope.foregroundIndicator(
+    sweepAngle: Float,
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = sweepAngle,
         useCenter = false,
         style = Stroke(
             width = indicatorStrokeWidth,
--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,12 +3,25 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +40,27 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        CustomComponent()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember {
+            mutableIntStateOf(0)
+        }
+
+        CustomComponent(
+            indicatorValue = text,
+            maxValue = 1000
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        TextField(
+            value = text.toString(),
+            onValueChange = { text = if (it.isNotEmpty()) it.toInt() else 0 },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            maxLines = 1
+        )
     }
 }
