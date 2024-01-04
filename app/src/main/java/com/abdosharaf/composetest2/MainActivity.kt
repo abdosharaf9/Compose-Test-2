050f7e9f516e67c2fc4a9c5d3d0447ef0d9c6b24
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-04 20:27:50 +0200

    isScrolled - Useful LazyListState Extension Property

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,8 +3,28 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +43,38 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
-//    ColorPicker()
-    ImageColorPickerComponent()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        val list = remember {
+            List(size = 25) { it }
+        }
+        val state = rememberLazyListState()
+
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            state = state,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            items(items = list, key = { it }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                        .clip(RoundedCornerShape(20.dp))
+                        .background(Color.LightGray)
+                )
+            }
+        }
+
+        Text(
+            text = if(state.isScrolled) "Scrolled" else "Idle",
+            color = if(state.isScrolled) Color.Red else Color.Black,
+            fontSize = 30.sp
+        )
+    }
 }
+
+val LazyListState.isScrolled: Boolean
+    get() = firstVisibleItemIndex > 0 || firstVisibleItemScrollOffset > 0
