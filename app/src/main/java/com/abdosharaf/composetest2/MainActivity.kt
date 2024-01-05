7a0d7d1b7d62093fe0186df4d46b736556cb4391
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-05 03:57:29 +0200

    Apply a Blur Effect on any Component

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.abdosharaf.composetest2
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.Companion.isPhotoPickerAvailable
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,12 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,66 +41,27 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
-    val context = LocalContext.current
-    var imageUri: Any? by remember {
-        mutableStateOf(R.drawable.image)
+    var checked by remember {
+        mutableStateOf(true)
     }
-
-    val photoPicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia()
-    ) { uri ->
-        if (uri != null) {
-            Log.d("```TAG```", "MainScreen: $uri")
-            imageUri = uri
-        } else {
-            Log.d("```TAG```", "MainScreen: No selected media")
-        }
-    }
-
-    val multiplePhotosPicker = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 3)
-    ) { list ->
-        if (list.isNotEmpty()) {
-            Log.d("```TAG```", "MainScreen: $list")
-        } else {
-            Log.d("```TAG```", "MainScreen: No selected media")
-        }
-    }
+    val animatedBlur by animateDpAsState(targetValue = if (checked) 10.dp else 0.dp, label = "")
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            modifier = Modifier
-                .size(250.dp)
-                .clickable {
-                    photoPicker.launch(
-                        PickVisualMediaRequest(
-                            ActivityResultContracts.PickVisualMedia.ImageOnly
-                        )
-                    )
-                },
-            model = ImageRequest.Builder(context)
-                .data(imageUri)
-                .crossfade(true)
-                .build(),
-            contentDescription = "Image",
-            contentScale = ContentScale.Crop
+        Text(
+            text = "Hello World!",
+            fontSize = MaterialTheme.typography.displayLarge.fontSize,
+            modifier = Modifier.blur(
+                radius = animatedBlur,
+                edgeTreatment = BlurredEdgeTreatment.Unbounded
+            )
         )
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        Button(onClick = {
-            val isAvailable = isPhotoPickerAvailable(context)
-            Toast.makeText(
-                context,
-                if (isAvailable) "Available" else "Not Available",
-                Toast.LENGTH_SHORT
-            ).show()
-        }) {
-            Text(text = "Check Availability")
-        }
+        Switch(checked = checked, onCheckedChange = { checked = !checked })
     }
 }
