dedd8170c2b13fc6e960b8028329e297c9f6f39d
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-06 21:41:56 +0200

    Beautiful Gallery Transition with Horizontal Pager

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -1,28 +1,34 @@
 package com.abdosharaf.composetest2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,34 +45,62 @@ fun DefaultPreview() {
     MainScreen()
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MainScreen() {
-    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.animation))
-    var isPlaying by remember { mutableStateOf(true) }
-
-    val progress by animateLottieCompositionAsState(
-        composition = composition,
-        isPlaying = isPlaying
-    )
-
-    LaunchedEffect(key1 = progress) {
-        if (progress == 0f) isPlaying = true
-        if (progress == 1f) isPlaying = false
-    }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        LottieAnimation(
-            composition = composition,
-            progress = { progress },
-            modifier = Modifier.size(400.dp)
+    val images = remember {
+        mutableStateListOf(
+            "https://images.pexels.com/photos/18273081/pexels-photo-18273081/free-photo-of-scenic-cliff-at-stokksnes-on-iceland.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/10033269/pexels-photo-10033269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/16794803/pexels-photo-16794803/free-photo-of-building-on-a-pier.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/15091952/pexels-photo-15091952/free-photo-of-woman-near-the-lake-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/11182271/pexels-photo-11182271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/19716967/pexels-photo-19716967/free-photo-of-a-house-with-a-blue-door-and-a-tree-in-front.jpeg"
         )
+    }
+    val pagerState = rememberPagerState()
+    val matrix = remember { ColorMatrix() }
 
-        Button(onClick = { isPlaying = true }) {
-            Text(text = "Play Again")
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 48.dp)
+    ) {
+        HorizontalPager(
+            count = images.size,
+            state = pagerState
+        ) { index ->
+            Log.d("```TAG```", "MainScreen: ${pagerState.currentPageOffset}")
+            val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffset
+            val imageSize by animateFloatAsState(
+                targetValue = if (pageOffset != 0.0f) 0.75f else 1f,
+                animationSpec = tween(durationMillis = 300),
+                label = ""
+            )
+
+            LaunchedEffect(key1 = imageSize) {
+                Log.d("```TAG```", "MainScreen: Size $imageSize")
+                if (pageOffset != 0.0f) matrix.setToSaturation(0f)
+                else matrix.setToSaturation(1f)
+            }
+
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .graphicsLayer {
+                        scaleX = imageSize
+                        scaleY = imageSize
+                    },
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(images[index])
+                    .build(),
+                contentDescription = "Image",
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.colorMatrix(matrix)
+            )
         }
     }
 }
