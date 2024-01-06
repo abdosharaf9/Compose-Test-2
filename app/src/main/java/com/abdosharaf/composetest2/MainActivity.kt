35f7635882e8c0bfff1e2608e463e9eb235918f2
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-06 15:18:02 +0200

    derivedStateOf

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.abdosharaf.composetest2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,22 +36,25 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        var isVisible by remember {
-            mutableStateOf(false)
+    var count by remember { mutableIntStateOf(0) }
+    val calculation by remember {
+        derivedStateOf {
+            Log.d("```TAG```", "MainScreen: Calculated")
+            count > 3
         }
+    }
+    Log.d("```TAG```", "MainScreen: $calculation")
 
-        Text(modifier = Modifier.testTag("Hello"), text = "Hello!")
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(modifier = Modifier.testTag("Button"), onClick = { isVisible = !isVisible }) {
-            Text(text = if (isVisible) "Hide" else "Show")
-        }
-
-        if (isVisible) {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(modifier = Modifier.testTag("Hi"), text = "Hi!")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            Log.d("```TAG```", "MainScreen: Clicked")
+            count++
+        }) {
+            Text(text = "Increase")
         }
     }
 }
