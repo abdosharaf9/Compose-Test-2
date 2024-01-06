4b29bf59f66965bca78fbb6d320374928a97e0c7
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-06 16:21:59 +0200

    Moving Animation with Marquee Effect

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,26 +3,31 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.MarqueeAnimationMode
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.abdosharaf.composetest2.ui.theme.ComposeTest2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,36 +44,66 @@ fun DefaultPreview() {
     MainScreen()
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen() {
-    var darkTheme by remember {
-        mutableStateOf(false)
-    }
-
-    ComposeTest2Theme(darkTheme = darkTheme) {
-        Content(darkTheme) { darkTheme = !darkTheme }
-    }
-}
-
-@Composable
-fun Content(darkTheme: Boolean, onThemeUpdated: () -> Unit) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surface),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { }) {
-            Text(text = "Test")
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+                .basicMarquee(
+                    iterations = Int.MAX_VALUE,
+                    animationMode = MarqueeAnimationMode.Immediately,
+                    velocity = 100.dp,
+                    initialDelayMillis = 0,
+                    delayMillis = 0
+                ),
+            text = "Nick Denton got drudged 5 praise erasers & how to avoid them 5% corruption trolls NPR future of context curmudgeon NYT R&D attracting young readers, Gutenberg parenthesis Sulzberger Quora newsroom cafe Walter Cronkite died for your sins mthomps What Would Google Do This Week in Review, Quora reporting crowdsourcing Politics & Socks page Jeff Jarvis bloggers in their mother's basement morgue future of narrative. vast wasteland the other longer Book Review Dan Fleckner Andy Carvin Fuego people formerly known as the audience explainer The Printing Press as an Agent of Change hyperhyperlocal social media optimization the medium is the massage, MinnPost AP Josh Marshall DocumentCloud abundance Jurgen Habermas Lucius Nieman tools crowdsourcing recontextualize, net neutrality AP the power of the press belongs to the person who owns one fourth estate Reuters experiment Gardening & War section MinnPost explainer.",
+            color = Color.White,
+            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            maxLines = 1,
+            overflow = TextOverflow.Visible
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        val focusRequester = remember {
+            FocusRequester()
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .basicMarquee(
+                    iterations = Int.MAX_VALUE,
+                    animationMode = MarqueeAnimationMode.WhileFocused,
+                    velocity = 150.dp,
+                    delayMillis = 0
+                )
+                .focusRequester(focusRequester)
+                .focusable()
+        ) {
+            Text(
+                text = "\uD83D\uDE82",
+                fontSize = MaterialTheme.typography.titleLarge.fontSize
+            )
+
+            repeat(20) {
+                Text(
+                    text = "🚃",
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize
+                )
+            }
         }
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Tessssssssssst", color = MaterialTheme.colorScheme.primary)
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Switch(checked = darkTheme, onCheckedChange = { onThemeUpdated() })
+        Button(onClick = { focusRequester.requestFocus() }) {
+            Text(text = "Woo-hoo!")
+        }
     }
 }
