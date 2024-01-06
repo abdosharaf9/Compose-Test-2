bb7cf0cfc9b4bf12e102c940ef2cb8b941dc013e
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-06 13:34:09 +0200

    Whats new in Material 3 TopAppBar

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -3,24 +3,23 @@ package com.abdosharaf.composetest2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Switch
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.blur
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -41,27 +40,33 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
-    var checked by remember {
-        mutableStateOf(true)
-    }
-    val animatedBlur by animateDpAsState(targetValue = if (checked) 10.dp else 0.dp, label = "")
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Hello World!",
-            fontSize = MaterialTheme.typography.displayLarge.fontSize,
-            modifier = Modifier.blur(
-                radius = animatedBlur,
-                edgeTreatment = BlurredEdgeTreatment.Unbounded
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Test")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+                    }
+                },
+                backgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
             )
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Switch(checked = checked, onCheckedChange = { checked = !checked })
+        }
+    ) { padding ->
+        val numbers = (1..20).toList()
+        LazyColumn(modifier = Modifier.padding(padding)) {
+            items(items = numbers, key = { it }) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    text = "$it",
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
     }
 }
