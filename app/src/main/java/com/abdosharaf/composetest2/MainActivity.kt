22a2a2412c6902c39d022649a6b8ec47b5490a94
Abdo Sharaf <bodyasharaf8@gmail.com>
2024-01-06 23:04:19 +0200

    Edge to Edge

--- app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest2/MainActivity.kt
@@ -1,30 +1,42 @@
 package com.abdosharaf.composetest2
 
+import android.annotation.SuppressLint
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFontFamilyResolver
-import androidx.compose.ui.text.font.createFontFamilyResolver
 import androidx.compose.ui.tooling.preview.Preview
-import com.abdosharaf.composetest2.ui.theme.fontFamily
-import kotlinx.coroutines.CoroutineExceptionHandler
+import com.abdosharaf.composetest2.ui.theme.ComposeTest2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                TRANSPARENT, TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                TRANSPARENT, TRANSPARENT
+            )
+        )
+
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            ComposeTest2Theme {
+                MainScreen()
+            }
         }
     }
 }
@@ -35,26 +47,20 @@ fun DefaultPreview() {
     MainScreen()
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    val handler = CoroutineExceptionHandler { _, throwable ->
-        Log.e("```TAG```", "There has been an issue: ", throwable)
-    }
-
-    CompositionLocalProvider(
-        LocalFontFamilyResolver provides createFontFamilyResolver(LocalContext.current, handler)
-    ) {
-        Box(
+    Scaffold {
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.surface),
-            contentAlignment = Alignment.Center
+                .statusBarsPadding()
+                .navigationBarsPadding(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Hello!",
-                fontFamily = fontFamily,
-                fontSize = MaterialTheme.typography.displayLarge.fontSize
-            )
+            Text(text = "Hello")
+            Text(text = "Hello")
         }
     }
 }
